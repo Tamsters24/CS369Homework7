2c9964d1ea06ccd29ec270fe6a919f10aa3e0e06
Tamsters24 <102896617+Tamsters24@users.noreply.github.com>
2023-02-22 19:02:39 -0800

    Buttons 7, 8, 9 working

--- app/src/main/java/com/example/tbusche_calculator/MainActivity.kt
+++ app/src/main/java/com/example/tbusche_calculator/MainActivity.kt
@@ -2,10 +2,25 @@ package com.example.tbusche_calculator
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.EditText
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    fun concatNumber(view: View) {
+        val calcDisplay = findViewById<EditText>(R.id.calculator_display)
+        var displayNumber = calcDisplay.text.toString()
+        val numberbtn = view as Button
+        when (numberbtn.id) {
+            R.id.button7 -> displayNumber += "7"
+            R.id.button8 -> displayNumber += "8"
+            R.id.button9 -> displayNumber += "9"
+        }
+        calcDisplay.setText(displayNumber)
     }
 }
