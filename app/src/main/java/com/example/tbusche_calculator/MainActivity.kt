c7a2bc713c46d4f79c45d3f65a32b8369a88cee9
Tamsters24 <102896617+Tamsters24@users.noreply.github.com>
2023-02-22 19:10:30 -0800

    Buttons 0 thru 9 and decimal working

--- app/src/main/java/com/example/tbusche_calculator/MainActivity.kt
+++ app/src/main/java/com/example/tbusche_calculator/MainActivity.kt
@@ -17,9 +17,21 @@ class MainActivity : AppCompatActivity() {
         var displayNumber = calcDisplay.text.toString()
         val numberbtn = view as Button
         when (numberbtn.id) {
+            R.id.button1 -> displayNumber += "1"
+            R.id.button2 -> displayNumber += "2"
+            R.id.button3 -> displayNumber += "3"
+            R.id.button4 -> displayNumber += "4"
+            R.id.button5 -> displayNumber += "5"
+            R.id.button6 -> displayNumber += "6"
             R.id.button7 -> displayNumber += "7"
             R.id.button8 -> displayNumber += "8"
             R.id.button9 -> displayNumber += "9"
+            R.id.button0 -> displayNumber += "0"
+            R.id.decimal_button -> {
+                if (displayNumber.contains(".")) { /* do nothing */ }
+                else
+                    displayNumber += "."
+            }
         }
         calcDisplay.setText(displayNumber)
     }
