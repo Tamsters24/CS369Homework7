cb8529f22419e4a9be563640e4dda965f07fda6b
Tamsters24 <102896617+Tamsters24@users.noreply.github.com>
2023-02-24 18:01:19 -0800

    Everything working except percent

--- app/src/main/java/com/example/tbusche_calculator/MainActivity.kt
+++ app/src/main/java/com/example/tbusche_calculator/MainActivity.kt
@@ -7,26 +7,37 @@ import android.widget.EditText
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
-    //private val displayTop: EditText = findViewById(R.id.calc_display_top)          /* Access the top display */
-    //private val displayBottom: EditText = findViewById(R.id.calc_display_bottom)    /* Access the bottom display */
-    private var storedExpression = ""
+    /* There is a top display for storing previously entered expressions
+     * while the bottom display contains the current entry. Meanwhile,
+     * equations will have a first operand stored (until cleared), and
+     * when an operation is performed, a second operand from the bottom
+     * display value is used. The two operands are calculated according
+     * to the operator indicated. This continues until the display is
+     * cleared. */
+    private var topExpression = ""
     private var operand1: Float = 0F
     private var operand2: Float = 0F
     private var operator: String = ""
 
     override fun onCreate(savedInstanceState: Bundle?) {
+        /* The initial screen displays "0" in the bottom
+         * display and nothing in the top display */
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
 
-    fun concatNumber(view: View) {                                          /* Comments */
+    fun concatNumber(view: View) {
         val displayBottom: EditText = findViewById(R.id.calc_display_bottom)
-        var displayNumber = displayBottom.text.toString()                   /* Convert the display text to string */
-        val numberBtn = view as Button                                      /* Access the button clicked */
-        if (displayNumber == "0")                                           /* Initial display shows 0 */
-            displayNumber = ""                                              /* Remove initial display for new value */
-        when (numberBtn.id) {                                               /* Switch for button clicked */
-            R.id.button1 -> displayNumber += "1"                            /* 1 thru 9 is appended to display text */
+        var displayNumber = displayBottom.text.toString()
+        val numberBtn = view as Button
+
+        /* If on an initial screen, Replace "0" on the bottom display with
+         * the new value to be concatenated. */
+        if (displayNumber == "0")
+            displayNumber = ""
+        /* Concatenate integers or decimal */
+        when (numberBtn.id) {
+            R.id.button1 -> displayNumber += "1"
             R.id.button2 -> displayNumber += "2"
             R.id.button3 -> displayNumber += "3"
             R.id.button4 -> displayNumber += "4"
@@ -36,17 +47,17 @@ class MainActivity : AppCompatActivity() {
             R.id.button8 -> displayNumber += "8"
             R.id.button9 -> displayNumber += "9"
             R.id.button0 -> displayNumber += "0"
-            R.id.decimal_button -> {                                        /* Only accept one decimal to append */
+            R.id.decimal_button -> {    /* Only accept one decimal to append */
                 if (displayNumber.contains(".")) { /* do nothing */ }
                 else if (displayNumber == "")
                     displayNumber = "0."
                 else
                     displayNumber += "."
             }
-            R.id.pos_neg_button -> {                                        /* Switch between negative and positive */
+            R.id.pos_neg_button -> {    /* Switch between negative and positive */
                 displayNumber =
                     if (displayNumber[0] == '-')
-                        displayNumber.drop(1)   // Odd. It doesn't work as intended for n = 0
+                        displayNumber.drop(1)   // Odd. It doesn't work with n = 0
                     else
                         "-$displayNumber"
             }
@@ -54,10 +65,10 @@ class MainActivity : AppCompatActivity() {
         displayBottom.setText(displayNumber)
     }
 
-    fun delete(view: View) {                                                /* Because the delete button is     */
-        val displayBottom: EditText = findViewById(R.id.calc_display_bottom)
-        var displayNumber = displayBottom.text.toString()                   /* not a button but an ImageButton, */
-        displayNumber =                                                     /* cannot use the previous switch   */
+    fun delete(view: View) {                                                    /* Because the delete button is     */
+        val displayBottom: EditText = findViewById(R.id.calc_display_bottom)    /* not a button but an ImageButton, */
+        var displayNumber = displayBottom.text.toString()                       /* cannot use the concat switch     */
+        displayNumber =
             if (displayNumber.length > 1)
                 displayNumber.subSequence(0, displayNumber.length - 1) as String
             else
@@ -65,60 +76,84 @@ class MainActivity : AppCompatActivity() {
         displayBottom.setText(displayNumber)
     }
 
-    fun clear(view: View) {                                                 /* Resets display to initial display */
-        val displayTop: EditText = findViewById(R.id.calc_display_top)
-        val displayBottom: EditText = findViewById(R.id.calc_display_bottom)
-        storedExpression = ""
-        displayTop.setText("")
-        displayBottom.setText("0")
-    }
-
     fun numberOperation(view: View) {
-        /* There is a top display for storing previously entered expressions
-         * while the bottom display contains the current entry. When an operation
-         * is clicked: 1) store the current value subsequent calculation. 2) store
-         * the operand for when the calculation is executed. 3) concatenate the
-         * stored expression with values and operator symbols. 4) Update the top
-         * display with the stored expression. 5) Refresh the bottom display. */
+        /* When an operation is clicked:
+         * 1) store the current value for subsequent calculation.
+         * 2) store the operand for when the calculation is executed.
+         * 3) concatenate the stored expression with values and operator symbols.
+         * 4) update the top display with the stored expression.
+         * 5) Refresh the bottom display. */
+
         val displayTop: EditText = findViewById(R.id.calc_display_top)
         val displayBottom: EditText = findViewById(R.id.calc_display_bottom)
-        val currentExpression = displayBottom.text.toString()
         val operatorBtn = view as Button
 
-        // prevent operation if a second operand has not been entered
-        if (isNumeric(currentExpression)) {
-            /* If there is no operand1 yet, assign it and store it and the
-             * operator in the top display. Otherwise, calculate the current
-             * operands, and place result and new operator in the stored display
-             * (until "equals" is executed) */
-            if (storedExpression == "") {
-                operand1 = currentExpression.toFloat()
-                storedExpression += currentExpression
-            } else if (isNumeric(currentExpression)) {
-                operand2 = currentExpression.toFloat()
-                storedExpression = calculate(operand1, operand2, operator)
-                operand1 = storedExpression.toFloat()
+        /* If equals was last executed, replace the stored expression
+         * with the result and operand, then reset the bottom display*/
+        if (topExpression.contains("=")) {
+            topExpression = operand1.toString()
+            if (topExpression.endsWith(".0")) { // If the result is an integer, remove the trailing digit
+                topExpression = topExpression.dropLast(2)
             }
 
             when (operatorBtn.id) {
                 R.id.divide_button -> {
-                    storedExpression += " /"
+                    topExpression += " /"
                     operator = "/"
                 }
                 R.id.multiply_button -> {
-                    storedExpression += " *"
+                    topExpression += " *"
                     operator = "*"
                 }
                 R.id.subtract_button -> {
-                    storedExpression += " - "
+                    topExpression += " -"
                     operator = "-"
                 }
                 R.id.addition_button -> {
-                    storedExpression += " + "
+                    topExpression += " +"
                     operator = "+"
                 }
             }
-            displayTop.setText(storedExpression)
+            displayTop.setText(topExpression)
+            displayBottom.setText("")
+        }
+
+        /* Otherwise, proceed normally */
+        val currentExpression = displayBottom.text.toString()
+        // prevent operation if a second operand has not been entered
+        if (isNumeric(currentExpression)) {
+            /* If there is no operand1 yet, assign it and store it and the
+             * operator in the top display. */
+            if (topExpression == "") {
+                operand1 = currentExpression.toFloat()
+                topExpression += currentExpression
+            }
+            /* Otherwise, calculate the current operands, and place result
+             * and new operator in the stored display (until "equals" is executed) */
+            else {
+                operand2 = currentExpression.toFloat()
+                topExpression = calculate(operand1, operand2, operator)
+                operand1 = topExpression.toFloat()
+            }
+            when (operatorBtn.id) {
+                R.id.divide_button -> {
+                    topExpression += " /"
+                    operator = "/"
+                }
+                R.id.multiply_button -> {
+                    topExpression += " *"
+                    operator = "*"
+                }
+                R.id.subtract_button -> {
+                    topExpression += " -"
+                    operator = "-"
+                }
+                R.id.addition_button -> {
+                    topExpression += " +"
+                    operator = "+"
+                }
+            }
+            displayTop.setText(topExpression)
             displayBottom.setText("")
         }
     }
@@ -128,71 +163,28 @@ class MainActivity : AppCompatActivity() {
          * and shows the result in the bottom display */
         val displayTop: EditText = findViewById(R.id.calc_display_top)
         val displayBottom: EditText = findViewById(R.id.calc_display_bottom)
-        val currentValue = displayBottom.text.toString()
 
-        /* If there is no operand1 yet, display "*current value* =" in the top display.
-         * Otherwise, display "*operand1* *operator* *operand2* =" in the top display and
-         * then assign the result to storedExpression */
-        if (storedExpression == "") {
-            operand1 = currentValue.toFloat()
-            storedExpression += currentValue
-            storedExpression += " ="
-            displayTop.setText(storedExpression)
-            displayBottom.setText("")
-        } else if (isNumeric(currentValue)) {
-            operand2 = currentValue.toFloat()
-            var topExpression = "$storedExpression $currentValue"
-            storedExpression = calculate(operand1, operand2, operator)
-            operand1 = storedExpression.toFloat()
-            topExpression = "$topExpression = "
-            displayTop.setText(topExpression)
-            displayBottom.setText(storedExpression)
+        /* Values for the top display's equation */
+        var leftValue = operand1.toString()
+        // If the left value is an integer, parse the digits
+        if (leftValue.endsWith(".0")) {
+            leftValue = leftValue.dropLast(2)
         }
-    }
+        val rightValue = displayBottom.text.toString()
 
-
-    /* I've always found the percent button confusing. So I'm using the following
-     * webpage as a guide:
-     * https://www.how2shout.com/how-to/how-to-calculate-percentage-in-calculator-with-a-percentage-key.html */
-    /*fun percent(view: View) {
-        val storeddisplay = findViewById<EditText>(R.id.calc_display_top)
-        val currentdisplay = findViewById<EditText>(R.id.calc_display_bottom)
-        val currentexpression = currentdisplay.text.toString()
-
-        if (storedExpression == "") {
-            currentdisplay.setText("0")
+        /* If there is no operand1 yet, do nothing */
+        if (topExpression == "") { /* Do nothing */ }
+        /* Otherwise, display "*operand1* *operator* *operand2* = result"
+         * in the top display and then assign the result to topExpression */
+        else if (isNumeric(rightValue)) {
+            operand2 = rightValue.toFloat()
+            val currentResult = calculate(operand1, operand2, operator)
+            val topView = "$leftValue $operator $rightValue = $currentResult"
+            displayTop.setText(topView)
+            operand1 = currentResult.toFloat()
+            topExpression = topView
+            displayBottom.setText(currentResult)
         }
-        else if (isNumeric(currentexpression)) {
-            operand1 = storedExpression.toDouble()
-            operand2 = currentexpression.toDouble()
-            when (operator) {
-                "/" -> {
-                    operand2 /= 100
-                    storedExpression = calculate(operand1, operand2, operator)
-                }
-                "*" -> {
-                    operand2 /= 100
-                    storedExpression = calculate(operand1, operand2, operator)
-                }
-                "-" -> {
-                    operand2 = operand1 * (operand2/100)
-                    storedExpression = calculate(operand1, operand2, operator)
-                }
-                "+" -> {
-                    operand2 = operand1 * (operand2/100)
-                    storedExpression = calculate(operand1, operand2, operator)
-                }
-            }
-            operand1 = storedExpression.toDouble()
-            operand2 = 0.0
-            operator = ""
-            storeddisplay.setText(storedExpression)
-            currentdisplay.setText("")
-        }
-    }*/
-
-    private fun isNumeric(string: String): Boolean {
-        return string.toDoubleOrNull() != null
     }
 
     private fun calculate(operandOne: Float, operandTwo: Float, operator:String): String {
@@ -203,6 +195,26 @@ class MainActivity : AppCompatActivity() {
             "-" -> result = (operandOne - operandTwo).toString()
             "+" -> result = (operandOne + operandTwo).toString()
         }
+        if (result.endsWith(".0")) {    // If the result is an integer, parse the digits
+            result = result.dropLast(2)
+        }
+        operand1 = result.toFloat()
         return result
     }
+
+    fun clear(view: View) {                                         /* Resets all values and initializes display */
+        val displayTop: EditText = findViewById(R.id.calc_display_top)
+        val displayBottom: EditText = findViewById(R.id.calc_display_bottom)
+        topExpression = ""
+        operand1 = 0F
+        operand2 = 0F
+        operator = ""
+        displayTop.setText("")
+        displayBottom.setText("0")
+    }
+
+    private fun isNumeric(string: String): Boolean {
+        return string.toDoubleOrNull() != null
+    }
+
 }
